a5e5b8e66ef356f18fbc961f27cf729d98f66b3a
IdentalerMaxima <Koppany.naray@gmail.com>
2022-06-08 16:38:08 +0200

    Action listener for window 1 implemented

--- src/Window.java
+++ src/Window.java
@@ -1,17 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements ActionListener {
     JFrame frame = new JFrame("Converter.V1");
-
     ImageIcon icon = new ImageIcon("converterIcon.jpg");
-
     JLabel text = new JLabel("What do you want to convert?");
     JButton buttonCurrency = new JButton("Currency");
     JButton buttonMetrics = new JButton("Metrics");
-
-
-
     public void createFrame(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -22,14 +19,22 @@ public class Window extends JFrame {
         frame.setIconImage(icon.getImage());
 
         buttonCurrency.setSize(100, 100);
+        buttonCurrency.addActionListener(this);
         buttonMetrics.setSize(100, 100);
+        buttonMetrics.addActionListener(this);
 
         frame.add(text, BorderLayout.NORTH);
         frame.add(buttonCurrency);
         frame.add(buttonMetrics);
-
     }
 
-
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == buttonCurrency){
+            System.out.println("Open curr. window");
+        }
+        if (e.getSource() == buttonMetrics){
+            System.out.println("Open metric window");
+        }
+    }
 }
